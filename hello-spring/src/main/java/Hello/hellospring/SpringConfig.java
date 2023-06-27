adebd95293b4507914cfb117c88c461398e0f1fb
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-06-27 11:10:35 +0900

    스프링 데이터 JPA

--- hello-spring/src/main/java/Hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/Hello/hellospring/SpringConfig.java
@@ -1,36 +1,31 @@
 package Hello.hellospring;
 
-import Hello.hellospring.repository.JdbcTemplateMemberRepository;
 import Hello.hellospring.repository.JpaMemberRepository;
 import Hello.hellospring.repository.MemberRepository;
-import Hello.hellospring.repository.MemoryMemberRepository;
 import Hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
-
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+    private final MemberRepository memberRepository;
 
     @Autowired
-
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
