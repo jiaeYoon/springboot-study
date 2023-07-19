b039c9fe22c03ce54d07bd9848489fad44b4ac8c
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-19 16:08:42 +0900

    Test : 스프링 빈 관련 테스트

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+class XmlAppContext {
+
+    @Test
+    @DisplayName("xml 기반 스프링 빈 설정 테스트")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
